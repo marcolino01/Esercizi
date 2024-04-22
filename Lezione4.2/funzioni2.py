f9b54ef9b608639676f2a9bdd7611d526c3cacd9
diciccomarco2001@gmail.com <diciccomarco2001@gmail.com>
2024-04-22 09:45:01 +0200

    lezione4.2

---
+++ Lezione4.2/funzioni2.py
@@ -0,0 +1,9 @@
+def subtract_all(x:list[float], y:float) -> list[float]:
+    # x = [1,2,3,4,5]
+    # y = [-4,-3,-2,-1,0]
+
+
+
+mylist : list[float] = [1,2,3,4,5]
+y : float = 10 
+result = subtract_all(mylist, y)
