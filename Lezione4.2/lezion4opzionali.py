bb1a100ea5824d6ec97df1aa7b927e719f1aa31b
diciccomarco2001@gmail.com <diciccomarco2001@gmail.com>
2024-04-30 16:42:32 +0200

    opzionali 2

--- Lezione4.2/lezion4opzionali.py
+++ Lezione4.2/lezion4opzionali.py
@@ -36,27 +36,45 @@ for i in students:
 #Terminate the loop when the user guesses the number correctly or reaches the maximum number of attempts.
 
 
-range_number : int = int(input("inserisci un numero per il range:->"))
+randomico : int = int(input("inserisci un numero per il range:->"))
 def guess_number(range_number):
     number : int = random.randint(0,range_number)
-    user_number : int = int(input("Hai tre tentativi per indovinare il numero.VIAAA"))
+    user_number : int = int(input("Hai tre tentativi per indovinare il numero.VIAAA "))
     count = 1
-    while i<3:
+    while count<3:
         if user_number != number:
-            user_number = int(input("Hai sbagliato ,riprova :->"))
-        elif number-10 < user_number < number+10:
-            if user_number > number:
-                user_number = int(input("Sei vicino riprova con un numero piu basso"))
+            if number-10 < user_number < number+10:
+                if user_number > number:
+                    user_number = int(input("Sei vicino riprova con un numero piu basso :->"))
+                    count+=1
+                else:
+                    user_number = int(input("Sei vicino riprova con un numero più alto :->"))
+                    count+=1
             else:
-                user_number = int(input("Sei vicino riprova con un numero più alto"))
+                if user_number > number:
+                    user_number = int(input("Sei lontano prova con un numero piu piccolo :->"))
+                    count+=1
+                else:
+                    user_number = int(input("Sei lontano prova con un numero piu grande :->"))
+                    count+=1
         elif user_number == number:
             print(f"complimenti hai indovinato il numero era : {number}")
             break
-        else:
-            if user_number > number:
-                user_number = int(input("Sei lontano prova con un numero piu piccol0"))
-            else:
-                user_number = int(input("Sei lontano prova con un numero piu grande"))
+    
 
-    count+=1
+guess_number(randomico)
+
+#3. E-commerce Shopping Cart:
+
+#Create a function that defines a product with a name, price, and quantity.
+# Create a function that manages the shopping cart, allowing the user to add, remove, and view products in the cart.
+#The function should calculate the cart total and apply any discounts or taxes.
+# Implement a for loop to iterate over the items in the cart and print detailed information about each product and the total.
+
+#4. Text Analysis:
+#Create a function that reads a text file and counts the number of occurrences of each word.
+#The function should print a report showing the most frequent words and their number of occurrences.
+#You can use a for loop to iterate over the words in the text and a dictionary to store the occurrences.
+#Implement error handling to handle missing files or other input issues.
+
 
