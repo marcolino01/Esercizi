3e806ff307adcdbb15a219251d812da7fa21f86f
diciccomarco2001@gmail.com <diciccomarco2001@gmail.com>
2024-05-09 20:12:14 +0200

    nn

--- lezione6/es_obbligatori.py
+++ lezione6/es_obbligatori.py
@@ -62,21 +62,42 @@ ristorante.increment_number_served(5)
 last_name, and then create several other attributes that are typically stored in a user 
 profile. Make a method called describe_user() that prints a summary of the user’s information. 
 Make another method called greet_user() that prints a personalized greeting to the user. 
-Create several instances representing different users, and call both methods for each user."""
+Create several instances representing different users, and call both methods for each user.
+9-5. Login Attempts: Add an attribute called login_attempts to your User class from 
+Exercise 9-3. Write a method called increment_login_attempts() that increments the value 
+of login_attempts by 1. Write another method called reset_login_attempts() that resets 
+the value of login_attempts to 0. Make an instance of the User class and call 
+increment_login_attempts() several times. Print the value of login_attempts to make sure 
+it was incremented properly, and then call reset_login_attempts(). Print login_attempts again
+ to make sure it was reset to 0."""
 
 class User:
     
-    def __init__(self, first_name: str, last_name: str, age:str, city: str):
+    def __init__(self, first_name: str,
+                 last_name: str,
+                 age:str, 
+                 city: str, 
+                 ):
         self.first_name: str = first_name
         self.last_name: str = last_name
         self.age : int = age
         self.city: str = city
+        self.login_attempts: int = 0
     
     def describe_user(self) -> str:
         print(f"nome: {self.first_name}, cognome: {self.last_name}, età: {self.age}, città {self.city}")
 
     def greeting_user(self) -> str:
         print(f"È fantastico rivederti {self.first_name,self.last_name}")
+
+    def increment_login_attempts(self) ->int:
+            self.login_attempts += 1
+            return self.login_attempts
+    
+    def reset_login_attempts(self) -> int:
+        if self.login_attempts != 0:
+            self.login_attempts = 0
+        return self.login_attempts
 
 user1 = User("Marco","Di Cicco",23,"Ladispoli")
 user2 = User("Gianni","Rossi",56,"Poggibonzi")
@@ -88,3 +109,31 @@ user2.describe_user()
 user2.greeting_user()
 user3.describe_user()
 user3.greeting_user()
+
+print(user1.increment_login_attempts())
+print(user1.increment_login_attempts())
+print(user1.reset_login_attempts())
+
+"""9-6. Ice Cream Stand: An ice cream stand is a specific kind of restaurant. Write a class 
+called IceCreamStand that inherits from the Restaurant class you wrote in Exercise 9-1  or 
+Exercise 9-4. Either version of the class will work; just pick the one you like better. Add 
+an attribute called flavors that stores a list of ice cream flavors. Write a method that 
+displays these flavors. Create an instance of IceCreamStand, and call this method. """
+
+class IceCreamStand:
+    def __init__(self, name : Restaurant, cuisine_type : Restaurant):
+        self.flavors : list[str] = []
+        self.name : Restaurant = name
+        self.cuisine_type : Restaurant = cuisine_type
+
+    def icecream_flavors(self,flavor : str) -> list[str]:
+        self.flavors.append(flavor)
+
+    def display_flavors(self)->str:
+        for flavor in self.flavors:
+            print(flavor)
+    
+gelateria : str = IceCreamStand("Gelateria","artigianale")
+
+gelateria.icecream_flavors("Fragola")
+gelateria
