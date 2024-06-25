c2c5dfe27d0859d02aae9bee8c0c8bd3f6c5acf5
diciccomarco2001@gmail.com <diciccomarco2001@gmail.com>
2024-06-25 15:09:16 +0200

    mm

--- lezione20/pagamento.py
+++ lezione20/pagamento.py
@@ -29,25 +29,28 @@ class PagamentoContanti(Pagamento):
         self.pezzi : dict[float, int] = {500.00 : 0, 200.00 : 0, 100.00 : 0, 50.00 : 0, 20.00 : 0, 10.00 : 0, 5.00 : 0, 2.00 : 0, 1.00 : 0,
                                          0.50 : 0, 0.20 : 0, 0.10 : 0, 0.05 : 0, 0.02 : 0, 0.01: 0}
         importo = self.getPagamento()
-
+        print(f"Pagamento in contanti di {self.getPagamento()}\n"
+                f"{self.getPagamento()} euro da pagare in:")
         while importo != 0:
             for k,v in self.pezzi.items():
-                if round(importo,2) > k:
-                    importo -= k
+                if round(importo,2) >= k:
+                    importo = round(importo-k,2)
                     self.pezzi[k] = 1 + v
                     break
-        ###for k,v in self.pezzi.items():
-            #if v > 0:
-                #if k >= 5:
-                #    print(f"{self.pezzi[k]} banconota da")
-                #else:
+
+        for k,v in self.pezzi.items():
+            if v > 0:
+                if k >= 5:
+                    print(f"{v} banconota da {k}")
+                else:
+                    print(f"{v} monete da {k}")
 
 class PagamentoCarteCredito(Pagamento):
     
-    def __init__(self, nome_titolare: str, data_di_scadenza: int, numero_carta: int ):
+    def __init__(self, nome_titolare: str, data_di_scadenza: str, numero_carta: int ):
         super().__init__()
         self.nome_titolare : str = nome_titolare
-        self.data_di_scadenza : int = data_di_scadenza
+        self.data_di_scadenza : str = data_di_scadenza
         self.numero_carta : int = numero_carta
 
     def dettagliPagamento(self):
